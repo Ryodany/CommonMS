c5638213fde1c2619037a31d7da7f1339ab390c4
Jesús Izquierdo <susej_ic@hotmail.com>
2022-02-17 09:24:48 +0100

    Retirar función basura
    Retiramos código remanente de pruebas anteriores

--- files/player.h
+++ files/player.h
@@ -3,28 +3,9 @@
 #include <iostream>
 #include <random>
 
-// Function to iterate through all values. I equals number of values in tuple
-template <size_t I = 0, typename... Ts>
-typename std::enable_if<I == sizeof...(Ts), void>::type	printTuple(std::tuple<Ts...> tup)
-{
-	// If iterated through all values of tuple, then simply return.
-	return;
-}
-
-template <size_t I = 0, typename... Ts>
-typename std::enable_if<(I < sizeof...(Ts)), void>::type printTuple(std::tuple<Ts...> tup)
-{
-	// Print element of tuple
-	std::cout << std::get<I>(tup) << " ";
-
-	// Go to next element
-	printTuple<I + 1>(tup);
-}
-
 class Player {
 
 protected:
-
 	std::vector <std::tuple<unsigned int, unsigned int, std::string, bool>> m_achievements;
 
 	Player& operator= (const Player& other) = delete;
